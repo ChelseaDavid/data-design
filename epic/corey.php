6a5434b87f13116accb5c5b7e69bb168ba1adf76
Chelsea David <cryan17@cnm.edu>
2018-07-11 22:10:41 -0600

    created persona

---
+++ epic/corey.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Persona</title>
+	</head>
+
+	<body>
+		<h1>Corey Snyder</h1>
+		<img src="corey.jpeg">
+		<br>
+		<p>
+			<strong>Name:</strong> Corey <br>
+			<strong>Gender:</strong> Male<br>
+			<strong>Age:</strong> 42<br>
+			<strong>Marital Status:</strong> Single<br>
+			<strong>Profession:</strong> IT professional at Verizon Wireless<br>
+			<strong>Income Range:</strong> $70k - 80k per year
+		</p>
+
+		<h1>Attitude:</h1>
+		<ul>
+			<li>Corey is a highly respected member throughout the gaming community. He is highly educated<br>
+				on the latest and greatest trends, platforms, games, consoles,and everything in between. From<br>
+				set up, to maintenance, to resolution, Corey is the man. And boy does he know it.<br>
+				Corey, though big headed, also knows how difficult things can get in the gaming community along with<br>
+				 the <em>many, many, many,</em> questions that accompany it. Corey enjoys <br>
+				any opportunity he has to help inform, correct, and educate people all around the world</li>
+		</ul>
+		<h1>Likes:</h1>
+		<ul>
+			<li>Helping people</li>
+			<li>Attending Comic-Con</li>
+			<li>Building computers</li>
+			<li>Video Games</li>
+		</ul>
+		<h1>Needs:</h1>
+		<ul>
+			<li>Corey needs to be notified when his answer/correction has been received</li>
+			<li> Corey also needs to be alerted if a response is made on the current thread </li>
+			<li> Corey also needs the ability to un-subscribe to a thread if needed</li>
+		</ul>
+		<h1>Frustrations:</h1>
+		<ul>
+			<li>He does not like having to be redirected to a log in page in order to leave comments, or answers<br>
+			to questions regarding different products offered </li>
+		</ul>
+		<h1>Technology:</h1>
+		<h3>Comfort level :</h3>
+		<ul>
+			<li>Very comfortable with technology and gaming due to interest & hobby, but also occupation</li>
+			<li>Uses Microsoft Xbox One S Starter Bundle ‑ 1 TB ‑ Robot White</li>
+			<li>Microsoft Xbox 360 E 4GB Console with Kinect Camera Bundle</li>
+			<li>Playstation 4 slim 1 TB</li>
+			<li>Super Nintendo</li>
+			<li>Atari</li>
+			<li>Nintendo Wii</li>
+			<li>Home computer- Linux</li>
+			<li>iPhone X iOS 11: 2436x1125px</li>
+		</ul>
+		<a href="index.php">Home</a>
+	</body>
+
+</html>
