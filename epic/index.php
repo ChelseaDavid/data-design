5b1f40526d2f63dfa2559b4b53e763624faa4dc0
Chelsea David <cryan17@cnm.edu>
2018-07-11 22:10:04 -0600

    added plain text nav html page

---
+++ epic/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Data Design</title>
+	</head>
+
+	<body>
+		<h1>Data Design</h1>
+		<ul>
+			<li><a href="corey.php">Persona</a></li>
+			<li><a href="use-case.php">Use Case & User Story</a></li>
+			<li><a href="conceptual-model.php">Conceptual Model</a></li>
+			<li><a href="conclusion.php">Conclusion</a></li>
+		</ul>
+	</body>
+
+</html>
