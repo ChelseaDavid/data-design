4e49cfa0685df7382734135a9753737bd8e2781f
Chelsea David <cryan17@cnm.edu>
2018-07-14 11:42:35 -0600

    added drop tables if exist

--- sql/data-design.sql
+++ sql/data-design.sql
@@ -1,3 +1,8 @@
 -- The statement below sets the collation of the database to utf8
 ALTER DATABASE cryan17 CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
+-- This will drop any currently existing tables --
+DROP TABLE IF EXISTS answer;
+DROP TABLE IF EXISTS question;
+DROP TABLE IF EXISTS profile;
+
