638bd1e4700ed6ccc5c98c227ee858388e5f5ae5
Chelsea David <cryan17@cnm.edu>
2018-07-14 11:44:10 -0600

    added answer table

--- sql/data-design.sql
+++ sql/data-design.sql
@@ -6,31 +6,6 @@ DROP TABLE IF EXISTS answer;
 DROP TABLE IF EXISTS question;
 DROP TABLE IF EXISTS profile;
 
--- This will drop any currently existing tables --
-DROP TABLE IF EXISTS answer;
-DROP TABLE IF EXISTS question;
-DROP TABLE IF EXISTS profile;
-
--- Creates profile table--
-CREATE TABLE profile (
-	-- this creates the attribute for the primary key
-	-- not null means the attribute is required!
-	profileId BINARY(16) NOT NULL,
-	-- to make something optional, exclude the not null
-	profileLocation CHAR(32),
-	profileNickname VARCHAR(32) NOT NULL,
-	profileEmail VARCHAR(128) NOT NULL,
-	-- to make sure duplicate data cannot exist, create a unique index
-	UNIQUE(profileEmail),
-	-- this officiates the primary key for the entity
-	PRIMARY KEY (profileId)
-);
-
--- This will drop any currently existing tables --
-DROP TABLE IF EXISTS answer;
-DROP TABLE IF EXISTS question;
-DROP TABLE IF EXISTS profile;
-
 -- Creates profile table--
 CREATE TABLE profile (
 	-- this creates the attribute for the primary key
@@ -60,4 +35,21 @@ CREATE TABLE question (
 	FOREIGN KEY(questionProfileId) REFERENCES profile(profileId),
 	-- and finally create the primary key
 	PRIMARY KEY(questionId)
+);
+
+-- create the like entity (a weak entity from an m-to-n for profile --> tweet)
+CREATE TABLE answer (
+	-- these are still foreign keys
+	answerProfileId BINARY(16) NOT NULL,
+	answerQuestionId BINARY(16) NOT NULL,
+	answerContent VARCHAR(500) NOT NULL,
+	answerDate DATETIME(6) NOT NULL,
+	-- index the foreign keys
+	INDEX(answerProfileId),
+	INDEX(answerQuestionId),
+	-- create the foreign key relations
+	FOREIGN KEY(answerProfileId) REFERENCES profile(profileId),
+	FOREIGN KEY(answerQuestionId) REFERENCES question(questionId),
+	-- finally, create a composite foreign key with the two foreign keys
+	PRIMARY KEY(answerProfileId, answerQuestionId)
 );
